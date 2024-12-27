3fd79657da6d6bcc998d2623201d67b177d57b31
Dan Egnor <egnor@ofb.net>
2024-12-27 01:44:15 -0800

    tweak

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,5 @@
 import '../public/style/eacs.css';
 
 export default function({ children }) {
-  return (
-    <html>
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+  return <html><body>{children}</body></html>;
 }
