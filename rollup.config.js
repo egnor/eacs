81678b62570c78b625840d4eb5e2a97444f25e0e
Dan Egnor <egnor@ofb.net>
2025-01-21 13:48:10 -0800

    use external module

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,36 @@
-import copy from "rollup-plugin-copy";
-import del from "rollup-plugin-delete";
-import dev from "rollup-plugin-dev";
 import fg from "fast-glob";
-import mdx from "@mdx-js/rollup";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
-import url from "@rollup/plugin-url";
+import rollupAutomountDOM from "rollup-plugin-automount-dom";
+import rollupCopy from "rollup-plugin-copy";
+import rollupDelete from "rollup-plugin-delete";
+import rollupDev from "rollup-plugin-dev";
+import rollupHTML from "@rollup/plugin-html";
+import rollupMDX from "@mdx-js/rollup";
+import { nodeResolve as rollupNodeResolve } from "@rollup/plugin-node-resolve";
+import rollupTerser from "@rollup/plugin-terser";
+import rollupURL from "@rollup/plugin-url";
 
-import htmlWrapper from "./rollup_html_wrapper.js";
+const isWatch = process.env.ROLLUP_WATCH === "true";
 
-export default {
-  input: fg.sync("*.page.mdx"),
+const configs = fg.sync("*.mdx").map((input) => ({
+  input,
+  jsx: { mode: "automatic", jsxImportSource: "jsx-dom" },
   output: { dir: "build.tmp", format: "es" },
   plugins: [
-    copy({ targets: [{ src: "public/*", dest: "build.tmp" }] }),
-    del({ targets: "build.tmp/*" }),
-    dev("build.tmp"),
-    htmlWrapper(),
-    mdx({ jsxImportSource: "jsx-dom" }),
-    nodeResolve(),
-    terser(),
-    url({ fileName: "assets/[name]-[hash][extname]" }),
+    rollupAutomountDOM(),
+    rollupHTML({ fileName: input.replace(/mdx$/, "html"), title: "" }),
+    rollupMDX({ jsxImportSource: "jsx-dom" }),
+    rollupNodeResolve(),
+    !isWatch && rollupTerser(),
+    rollupURL({ fileName: "assets/[name]-[hash][extname]" }),
   ],
-};
+}));
+
+// Plugins that should only run once
+configs[0].plugins = [
+  rollupCopy({ targets: [{ src: "public/*", dest: "build.tmp" }] }),
+  rollupDelete({ targets: "build.tmp/*" }),
+  rollupDev("build.tmp"),
+  ...configs[0].plugins,
+];
+
+export default configs;
