b2a217b82ef0016536465d3555f485782074a2c7
Dan Egnor <egnor@ofb.net>
2025-01-21 17:16:23 -0800

    spacing

--- page_layout.jsx
+++ page_layout.jsx
@@ -1,5 +1,5 @@
 export default (props) => {
   if (props.title) document.title = props.title;
   document.head.appendChild(<link rel="stylesheet" href="/style/eacs.css" />);
-  return <> {props.children} </>;
+  return <>{props.children}</>;
 }
