1ebaffaf1b48f499f73f51a2fc243fb6b4059e8e
Dan Egnor <egnor@ofb.net>
2025-03-10 09:24:42 -0700

    update

--- build.js
+++ build.js
@@ -7,55 +7,51 @@ import esbuildAddWrapper from "esbuild-plugin-add-wrapper";
 import esbuildMdx from "@mdx-js/esbuild";
 import { htmlPlugin as esbuildHtml } from "@craftamap/esbuild-plugin-html";
 import fg from "fast-glob";
-import recmaJsxIfFor from "recma-plugin-jsx-if-for";
-import process from "node:process";
+import recmaJsxIfFor from "recma-jsx-if-for";
 
 process.chdir(import.meta.dirname);
 
 const opts = new Command()
   .option("--debug", "Add debug logging")
-  .option("--no-minify", "Do not minify Javascript")
-  .option("--serve", "Run dev server")
+  .option("--dev", "Run dev server")
   .parse().opts();
 
-const pages = await fg(["*.mdx"]);
+const pages = await fg(["page.*"]);
 const outdir = "build.tmp";
+const jsxImportSource = "jsx-dom";
 
-const context = await esbuild.context({
+const esbuildOptions = {
   absWorkingDir: process.cwd(),
+  assetNames: "[dir]/[name]-[hash]",
   bundle: true,
-  define: { ESBUILD_LIVE: JSON.stringify(opts.serve || false) },
+  define: { ESBUILD_LIVE: JSON.stringify(opts.dev || false) },
   entryPoints: [...pages, { in: "eacs.css", out: "style/eacs" }],
-  jsxImportSource: "jsx-dom",
+  jsxImportSource,
   jsx: "automatic",
   loader: { ".otf": "copy" },
   logLevel: opts.debug ? "debug" : "info",
   metafile: true,
-  minify: opts.minify,
+  minify: !opts.dev,
   outdir,
   plugins: [
-    esbuildAddWrapper({
-      filter: /\.mdx/, loader: "jsx", wrapper: "./page_wrapper.jsx"
-    }),
-    esbuildMdx({
-      jsx: true, jsxImportSource: "jsx-dom", recmaPlugins: [recmaJsxIfFor]
-    }),
+    esbuildAddWrapper({ filter: /\.mdx/, wrapper: "./page_wrapper.jsx" }),
+    esbuildMdx({ jsx: true, jsxImportSource, recmaPlugins: [recmaJsxIfFor] }),
     esbuildHtml({
       files: pages.map(p => ({
         entryPoints: { includes(v) { return v.endsWith(p); } },
-        filename: p.replace(/\.mdx$/, ".html"),
+        filename: p.split(".").slice(1, -1).join(".") + ".html",
         inline: true,
       })),
     }),
   ],
-});
+};
 
 await deleteAsync(outdir);
 
-if (opts.serve) {
+if (opts.dev) {
+  const context = await esbuild.context(esbuildOptions);
   await context.watch();
   await context.serve({ host: "localhost", servedir: outdir });
 } else {
-  const result = await context.rebuild();
-  context.dispose();
+  await esbuild.build(esbuildOptions);
 }
